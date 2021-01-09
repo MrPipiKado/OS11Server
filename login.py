66fb0d2ff1f3fa1f8ae510ad8f97bacff0958a42
MrPipiKado <ammamm23102310@gmail.com>
2021-01-09 21:11:03 +0200

    somthing done

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-import sqlite3
+import pymysql
 import socket
 import re
 from _thread import *
@@ -12,15 +12,16 @@ def check_if_user_exists(username, password):
     :param password: the password of the chat user
     :return: True, if record exists, False if not
     """
-    conn = sqlite3.connect('chat.db')
+    conn = pymysql.connect(host="127.0.0.1", user="chat", password="chat", database="chat", port=6603)
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE username = ? and password = ?", (username, password))
-    
+    cursor.execute("SELECT * FROM users WHERE username = %s and password = %s", (username, password))
+
     if cursor.fetchone() is not None:
         print(username + " " + password + " exists")
         return True
 
     print(username + " " + password + " does not exist")
+    conn.close()
     return False
 
 
@@ -37,29 +38,28 @@ def client_thread(conn, addr):
         if check_if_user_exists(user, passwd):
             conn.send(b"exists")
         else:
-            db = sqlite3.connect('chat.db')
+            db = pymysql.connect(host="127.0.0.1", user="chat", password="chat", database="chat", port=6603)
             cursor = db.cursor()
-            cursor.execute("INSERT INTO users(username, password, login) VALUES (?, ?, ?)", (user, passwd, True))
+            cursor.execute("INSERT INTO users(username, password, login) VALUES (%s, %s, %s)", (user, passwd, True))
             db.commit()
             print(user + " " + passwd + " added")
             conn.send(b"added")
-
+            db.close()
 
     remove(conn)
 
 
 # set the connection to database
-conn = sqlite3.connect('chat.db')
+conn = pymysql.connect(host="127.0.0.1", user="chat", password="chat", database="chat", port=6603)
 cursor = conn.cursor()
 
 # create the table with appropriate columns
 cursor.execute("""CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username VARCHAR(30),
-            password VARCHAR(30),
-            login BOOLEAN
-            )""")
-
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(30),
+  password VARCHAR(30),
+  login BOOLEAN
+) engine=InnoDB;""")
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 """
@@ -76,30 +76,31 @@ IP_address = str(sys.argv[1])
 Port = int(sys.argv[2])
 server.bind((IP_address, Port))
 
-#binds the server to an entered IP address and at the specified port number.The client must be aware of these parameters
+# binds the server to an entered IPaddress and at the specified port number.The client must be aware of these parameters
 server.listen(100)
 
-#listens for 100 active connections. This number can be increased as per convenience
-list_of_clients=[]
+# listens for 100 active connections. This number can be increased as per convenience
+list_of_clients = []
+
 
 def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove(connection)
 
+
 while True:
     conn, addr = server.accept()
     """
-    Accepts a connection request and stores two parameters, conn which is a socket object for that user, and addr which contains
+    Accepts a connection request and stores two parameters, conn which is a socket object for that user,
+    and addr which contains
     the IP address of the client that just connected
     """
     list_of_clients.append(conn)
     print(addr[0] + " connected")
-    #maintains a list of clients for ease of broadcasting a message to all available people in the chatroom
-    #Prints the address of the person who just connected
+    # maintains a list of clients for ease of broadcasting a message to all available people in the chatroom
+    # Prints the address of the person who just connected
     start_new_thread(client_thread, (conn, addr))
-    #creates and individual thread for every user that connects
-
-
+    # creates and individual thread for every user that connects
 
 server.close()
 conn.close()
